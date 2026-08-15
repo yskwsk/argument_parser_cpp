359016a546e89fa590288973d5e4e991568d4912
yoshihiro-kawasaki <kawayoshi.0117@gmail.com>
2026-08-16 00:20:47 +0900

    misc remove

--- misc/arg.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(const int argc, const char *argv[]) {
-    std::cout << argc << std::endl;
-    for (int i = 0; i < argc; ++i) {
-        std::cout << argv[i] << std::endl;
-    }
-    return 0;
-} 
